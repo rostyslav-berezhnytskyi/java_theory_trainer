c3c5c9831263027ded9dff5b3bc95d122e85d9a6
Rostyslav <rostik2510@gmail.com>
2026-06-30 18:56:13 +0300

    Initial commit

---
+++ src/main/java/com/els/javatheorytrainer/JavaTheoryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.els.javatheorytrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTheoryPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTheoryPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/els/javatheorytrainer/JavaTheoryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.els.javatheorytrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTheoryPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
